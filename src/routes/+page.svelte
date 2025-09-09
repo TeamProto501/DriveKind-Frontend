3c475b503e78a21bb8c11e5195a930f59a9dce86
Taewoong (Ricky) Um <rickyum527@gmail.com>
2025-09-09 00:28:58 -0400

    Change: Change main landing page so that it fits rest of the page when sidebar applied

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
   let testLoading = $state(false);
 </script>
 
-<div class="min-h-screen bg-gray-50 p-8">
+<div class="min-h-screen bg-gray-50 p-8 flex-1">
   <div class="max-w-4xl mx-auto">
     <h1 class="text-4xl font-bold text-gray-900 mb-8">Welcome to DriveKind</h1>
 
