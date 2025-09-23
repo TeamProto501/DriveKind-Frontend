a396d5be442ab7ce5eafa208c42f11cc51379522
Taewoong (Ricky) Um <rickyum527@gmail.com>
2025-09-22 22:46:17 -0400

    comment out loads

--- src/routes/admin/dash/+page.server.ts
+++ src/routes/admin/dash/+page.server.ts
@@ -1,18 +1,18 @@
 import { authenticatedFetch, API_BASE_URL } from "$lib/api";
 
 //initial data load to table
-export const load = async () => {
-  const res = await authenticatedFetch(`${API_BASE_URL}/clients/dash`);
+/* export const load = async () => {
+  const res = await authenticatedFetch(`${API_BASE_URL}clients/dash`);
   return { dashboard: res };
 };
 
 export const actions = {
   changeToVolunteer: async () => {
-    const res = await authenticatedFetch(`${API_BASE_URL}/volunteer/dash`);
+    const res = await authenticatedFetch(`${API_BASE_URL}volunteer/dash`);
     return { dashboard: res };
   },
   changeToDriver: async () => {
-    const res = await authenticatedFetch(`${API_BASE_URL}/driver/dash`);
+    const res = await authenticatedFetch(`${API_BASE_URL}driver/dash`);
     return { dashboard: res };
   },
-};
+}; */
