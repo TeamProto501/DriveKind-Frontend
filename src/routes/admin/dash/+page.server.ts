f4f760717fde1fa167fa6604d22273fae940c8ef
Taewoong (Ricky) Um <rickyum527@gmail.com>
2025-09-22 22:24:55 -0400

    ADD: action for loading dashboard

--- src/routes/admin/dash/+page.server.ts
+++ src/routes/admin/dash/+page.server.ts
@@ -2,6 +2,17 @@ import { authenticatedFetch, API_BASE_URL } from "$lib/api";
 
 //initial data load to table
 export const load = async () => {
-  const res = await authenticatedFetch(`${API_BASE_URL}/clients/dashboard`);
+  const res = await authenticatedFetch(`${API_BASE_URL}/clients/dash`);
   return { dashboard: res };
+};
+
+export const actions = {
+  changeToVolunteer: async () => {
+    const res = await authenticatedFetch(`${API_BASE_URL}/volunteer/dash`);
+    return { dashboard: res };
+  },
+  changeToDriver: async () => {
+    const res = await authenticatedFetch(`${API_BASE_URL}/driver/dash`);
+    return { dashboard: res };
+  },
 };
