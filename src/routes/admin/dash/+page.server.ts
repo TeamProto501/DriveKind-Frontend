1a56aed7be451ca3f819665e6bab0afad09d6b15
Taewoong (Ricky) Um <rickyum527@gmail.com>
2025-09-25 22:54:14 -0400

    ADD: add table configuration for dashboard

--- src/routes/admin/dash/+page.server.ts
+++ src/routes/admin/dash/+page.server.ts
@@ -1,22 +1,85 @@
 import { authenticatedFetch, API_BASE_URL } from "$lib/api";
 import type { AuthInfo } from "../../../lib/types";
 import type { RequestEvent } from "@sveltejs/kit";
+
+const Table_Config = {
+  clients: {
+    endpoint: "/clients/dash",
+    columns: [
+      { key: "first_name", label: "First Name", source: "first_name" },
+      { key: "last_name", label: "Last Name", source: "last_name" },
+      { key: "date_of_birth", label: "Date of Birth", source: "date_of_birth" },
+      {
+        key: "street_address",
+        label: "Street Address",
+        source: "street_address",
+      },
+      { key: "city", label: "City", source: "city" },
+      { key: "state", label: "State", source: "state" },
+      { key: "zip_code", label: "Zip_code", source: "zip_code" },
+      { key: "primary_phone", label: "Primary Phone", source: "primary_phone" },
+    ],
+  },
+  drivers: {
+    endpoint: "/driver/dash",
+    columns: [
+      { key: "first_name", label: "First Name", source: "first_name" },
+      { key: "last_name", label: "Last Name", source: "last_name" },
+      { key: "role", label: "Role", source: "role" },
+      { key: "email", label: "e-Mail", source: "email" },
+      { key: "dob", label: "Date of Birth", source: "dob" },
+      {
+        key: "street_address",
+        label: "Street Address",
+        source: "street_address",
+      },
+      { key: "city", label: "City", source: "city" },
+      { key: "state", label: "State", source: "state" },
+      { key: "zip_code", label: "Zip_code", source: "zip_code" },
+      { key: "primary_phone", label: "Primary Phone", source: "primary_phone" },
+    ],
+  },
+  volunteers: {
+    endpoint: "/volunteer/dash",
+    columns: [
+      { key: "first_name", label: "First Name", source: "first_name" },
+      { key: "last_name", label: "Last Name", source: "last_name" },
+      { key: "role", label: "Role", source: "role" },
+      { key: "email", label: "e-Mail", source: "email" },
+      { key: "dob", label: "Date of Birth", source: "dob" },
+      {
+        key: "street_address",
+        label: "Street Address",
+        source: "street_address",
+      },
+      { key: "city", label: "City", source: "city" },
+      { key: "state", label: "State", source: "state" },
+      { key: "zip_code", label: "Zip_code", source: "zip_code" },
+      { key: "primary_phone", label: "Primary Phone", source: "primary_phone" },
+    ],
+  },
+  dispatchers: {
+    endpoint: "/dispatcher/dash",
+    columns: [
+      { key: "first_name", label: "First Name", source: "first_name" },
+      { key: "last_name", label: "Last Name", source: "last_name" },
+      { key: "role", label: "Role", source: "role" },
+      { key: "email", label: "e-Mail", source: "email" },
+      { key: "dob", label: "Date of Birth", source: "dob" },
+      {
+        key: "street_address",
+        label: "Street Address",
+        source: "street_address",
+      },
+      { key: "city", label: "City", source: "city" },
+      { key: "state", label: "State", source: "state" },
+      { key: "zip_code", label: "Zip_code", source: "zip_code" },
+      { key: "primary_phone", label: "Primary Phone", source: "primary_phone" },
+    ],
+  },
+};
 //initial data load to table
 export const load = async (event) => {
-  /* const res = await authenticatedFetch(
-    `${API_BASE_URL}clients/dash`,
-    {},
-    undefined,
-    event
-  );
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch clients: ${res.status}`);
-  }
-
-  const clients = await res.json();
-  console.log(clients);
-  /*  return { clients }; */
   const res = await authenticatedFetch(
     `${API_BASE_URL}clients/dash`,
     {},
