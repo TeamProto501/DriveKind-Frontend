260db225afa35ff1df1585fae62e2d9cb83917d5
mcg4527 <mcg4527@rit.edu>
2025-09-27 16:52:42 -0400

    fix: users table and add users

---
+++ src/routes/admin/users/+page.server.ts
@@ -0,0 +1,15 @@
+// src/routes/admin/users/+page.server.ts
+import { authenticatedFetch, API_BASE_URL } from "$lib/api";
+
+export const load = async (event) => {
+  const res = await authenticatedFetch(
+    API_BASE_URL + "/staff/dash", // 👈 create a /staff/dash API endpoint
+    {},
+    undefined,
+    event
+  );
+  const text = await res.text();
+  const data = JSON.parse(text);
+
+  return { staffProfiles: data }; // passed as prop
+};
